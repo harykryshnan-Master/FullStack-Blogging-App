ba60651bedf74ed33382a7a0996707cf3b9a7e11
harykryshnan <harykryshnan27@gmail.com>
2025-01-18 18:24:31 +0000

    harykryshna-master-modify

--- EKS_Terraform/main.tf
+++ EKS_Terraform/main.tf
@@ -2,55 +2,55 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_vpc" "devopsshack_vpc" {
+resource "aws_vpc" "harykryshnan-master_vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "devopsshack-vpc"
+    Name = "harykryshnan-master-vpc"
   }
 }
 
-resource "aws_subnet" "devopsshack_subnet" {
+resource "aws_subnet" "harykryshnan-master_subnet" {
   count = 2
-  vpc_id                  = aws_vpc.devopsshack_vpc.id
-  cidr_block              = cidrsubnet(aws_vpc.devopsshack_vpc.cidr_block, 8, count.index)
+  vpc_id                  = aws_vpc.harykryshnan-master_vpc.id
+  cidr_block              = cidrsubnet(aws_vpc.harykryshnan-master_vpc.cidr_block, 8, count.index)
   availability_zone       = element(["ap-south-1a", "ap-south-1b"], count.index)
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "devopsshack-subnet-${count.index}"
+    Name = "harykryshnan-master-subnet-${count.index}"
   }
 }
 
-resource "aws_internet_gateway" "devopsshack_igw" {
-  vpc_id = aws_vpc.devopsshack_vpc.id
+resource "aws_internet_gateway" "harykryshnan-master_igw" {
+  vpc_id = aws_vpc.harykryshnan-master_vpc.id
 
   tags = {
-    Name = "devopsshack-igw"
+    Name = "harykryshnan-master-igw"
   }
 }
 
-resource "aws_route_table" "devopsshack_route_table" {
-  vpc_id = aws_vpc.devopsshack_vpc.id
+resource "aws_route_table" "harykryshnan-master_route_table" {
+  vpc_id = aws_vpc.harykryshnan-master_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.devopsshack_igw.id
+    gateway_id = aws_internet_gateway.harykryshnan-master_igw.id
   }
 
   tags = {
-    Name = "devopsshack-route-table"
+    Name = "harykryshnan-master-route-table"
   }
 }
 
-resource "aws_route_table_association" "a" {
+resource "aws_route_table_association" "harykryshnan-master_association" {
   count          = 2
-  subnet_id      = aws_subnet.devopsshack_subnet[count.index].id
-  route_table_id = aws_route_table.devopsshack_route_table.id
+  subnet_id      = aws_subnet.harykryshnan-master_subnet[count.index].id
+  route_table_id = aws_route_table.harykryshnan-master_route_table.id
 }
 
-resource "aws_security_group" "devopsshack_cluster_sg" {
-  vpc_id = aws_vpc.devopsshack_vpc.id
+resource "aws_security_group" "harykryshnan-master_cluster_sg" {
+  vpc_id = aws_vpc.harykryshnan-master_vpc.id
 
   egress {
     from_port   = 0
@@ -60,12 +60,12 @@ resource "aws_security_group" "devopsshack_cluster_sg" {
   }
 
   tags = {
-    Name = "devopsshack-cluster-sg"
+    Name = "harykryshnan-master-cluster-sg"
   }
 }
 
-resource "aws_security_group" "devopsshack_node_sg" {
-  vpc_id = aws_vpc.devopsshack_vpc.id
+resource "aws_security_group" "harykryshnan-master_node_sg" {
+  vpc_id = aws_vpc.harykryshnan-master_vpc.id
 
   ingress {
     from_port   = 0
@@ -82,25 +82,25 @@ resource "aws_security_group" "devopsshack_node_sg" {
   }
 
   tags = {
-    Name = "devopsshack-node-sg"
+    Name = "harykryshnan-master-node-sg"
   }
 }
 
-resource "aws_eks_cluster" "devopsshack" {
-  name     = "devopsshack-cluster"
-  role_arn = aws_iam_role.devopsshack_cluster_role.arn
+resource "aws_eks_cluster" "harykryshnan-master" {
+  name     = "harykryshnan-master-cluster"
+  role_arn = aws_iam_role.harykryshnan-master_cluster_role.arn
 
   vpc_config {
-    subnet_ids         = aws_subnet.devopsshack_subnet[*].id
-    security_group_ids = [aws_security_group.devopsshack_cluster_sg.id]
+    subnet_ids         = aws_subnet.harykryshnan-master_subnet[*].id
+    security_group_ids = [aws_security_group.harykryshnan-master_cluster_sg.id]
   }
 }
 
-resource "aws_eks_node_group" "devopsshack" {
-  cluster_name    = aws_eks_cluster.devopsshack.name
-  node_group_name = "devopsshack-node-group"
-  node_role_arn   = aws_iam_role.devopsshack_node_group_role.arn
-  subnet_ids      = aws_subnet.devopsshack_subnet[*].id
+resource "aws_eks_node_group" "harykryshnan-master" {
+  cluster_name    = aws_eks_cluster.harykryshnan-master.name
+  node_group_name = "harykryshnan-master-node-group"
+  node_role_arn   = aws_iam_role.harykryshnan-master_node_group_role.arn
+  subnet_ids      = aws_subnet.harykryshnan-master_subnet[*].id
 
   scaling_config {
     desired_size = 3
@@ -112,12 +112,12 @@ resource "aws_eks_node_group" "devopsshack" {
 
   remote_access {
     ec2_ssh_key = var.ssh_key_name
-    source_security_group_ids = [aws_security_group.devopsshack_node_sg.id]
+    source_security_group_ids = [aws_security_group.harykryshnan-master_node_sg.id]
   }
 }
 
-resource "aws_iam_role" "devopsshack_cluster_role" {
-  name = "devopsshack-cluster-role"
+resource "aws_iam_role" "harykryshnan-master_cluster_role" {
+  name = "harykryshnan-master-cluster-role"
 
   assume_role_policy = <<EOF
 {
@@ -135,13 +135,13 @@ resource "aws_iam_role" "devopsshack_cluster_role" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "devopsshack_cluster_role_policy" {
-  role       = aws_iam_role.devopsshack_cluster_role.name
+resource "aws_iam_role_policy_attachment" "harykryshnan-master_cluster_role_policy" {
+  role       = aws_iam_role.harykryshnan-master_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-resource "aws_iam_role" "devopsshack_node_group_role" {
-  name = "devopsshack-node-group-role"
+resource "aws_iam_role" "harykryshnan-master_node_group_role" {
+  name = "harykryshnan-master-node-group-role"
 
   assume_role_policy = <<EOF
 {
@@ -159,17 +159,18 @@ resource "aws_iam_role" "devopsshack_node_group_role" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "devopsshack_node_group_role_policy" {
-  role       = aws_iam_role.devopsshack_node_group_role.name
+resource "aws_iam_role_policy_attachment" "harykryshnan-master_node_group_role_policy" {
+  role       = aws_iam_role.harykryshnan-master_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "devopsshack_node_group_cni_policy" {
-  role       = aws_iam_role.devopsshack_node_group_role.name
+resource "aws_iam_role_policy_attachment" "harykryshnan-master_node_group_cni_policy" {
+  role       = aws_iam_role.harykryshnan-master_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
-resource "aws_iam_role_policy_attachment" "devopsshack_node_group_registry_policy" {
-  role       = aws_iam_role.devopsshack_node_group_role.name
+resource "aws_iam_role_policy_attachment" "harykryshnan-master_node_group_registry_policy" {
+  role       = aws_iam_role.harykryshnan-master_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
+
--- EKS_Terraform/output.tf
+++ EKS_Terraform/output.tf
@@ -1,15 +1,16 @@
 output "cluster_id" {
-  value = aws_eks_cluster.devopsshack.id
+  value = aws_eks_cluster.harykryshnan-master.id
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.devopsshack.id
+  value = aws_eks_node_group.harykryshnan-master.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.devopsshack_vpc.id
+  value = aws_vpc.harykryshnan-master_vpc.id
 }
 
 output "subnet_ids" {
-  value = aws_subnet.devopsshack_subnet[*].id
+  value = aws_subnet.harykryshnan-master_subnet[*].id
 }
+
